722f1cb16ec6633bd6e74f031d7a52ab0330e9d6
Paul Butcher <paul@paulbutcher.com>
2013-03-13 15:00:26 +0000

    Move to Clojure 1.5.1

--- project.clj
+++ project.clj
@@ -1,2 +1,6 @@
-(defproject foldable-seq "0.1"
-  :dependencies [[org.clojure/clojure "1.5.0"]])
+(defproject foldable-seq "0.2"
+  :description "Allow lazy sequences to be folded over in parallel"
+  :url "https://github.com/paulbutcher/foldable-seq"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
